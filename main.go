fa6450ac8d892c4339ed06a745026117ae6f99f4
Ben <ben.mwanzia@opibus.se>
2023-06-06 10:02:35 +0300

    telemetry api

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"bytes"
+	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"text/template"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -34,7 +37,30 @@ func main() {
 		port = "3000"
 	}
 
-	//myClient := &http.Client{Timeout: 10 * time.Second}
+	projectID := os.Getenv("GOOGLE_CLOUD_PROJECT")
+	if projectID == "" {
+		fmt.Println("GOOGLE_CLOUD_PROJECT environment variable must be set.")
+		os.Exit(1)
+	}
+
+	ctx := context.Background()
+
+	client, err := bigquery.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatalf("bigquery.NewClient: %v", err)
+	}
+	defer client.Close()
+
+	rows, err := query(ctx, client)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := printResults(os.Stdout, rows); err != nil {
+		log.Fatal(err)
+	}
+
+	myClient := &http.Client{Timeout: 10 * time.Second}
+	telemetryapi := telemetry.NewClient(myClient)
 
 	mux := http.NewServeMux()
 	fs := http.FileServer(http.Dir("assets"))
